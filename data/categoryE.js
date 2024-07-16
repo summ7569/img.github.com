334e7d79dafab41ee7ab0eb1fa0b0ca24058a003
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:26:27 +0900

    Create categoryE.js

---
+++ data/categoryE.js
@@ -0,0 +1,13 @@
+var Epositions = [
+    { category: '부림동', lat: 37.429, lng: 127.000 }
+];
+var EInfo = [
+    {
+        number: "HP-115",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    }
+];

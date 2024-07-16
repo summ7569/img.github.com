e21f7598eb1d57b59b40ec58df190198fdb6a4b2
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:24:25 +0900

    Update categoryA.js

--- data/categoryA.js
+++ data/categoryA.js
@@ -1,9 +1,25 @@
-const Apositions = [
+var Apositions = [
     { category: '갈현동', lat: 37.4249270000, lng: 126.9897680000 },
-    { category: '갈현동', lat: 37.423681, lng: 126.9929 }
+    { category: '갈현동', lat: 37.423681, lng: 126.9929 },
+    { category: '갈현동', lat: 37.422169, lng: 126.988136 },
+    { category: '갈현동', lat: 37.421404, lng: 126.987475 },
+    { category: '갈현동', lat: 37.419049, lng: 126.988793 },
+    { category: '갈현동', lat: 37.4182, lng: 126.9853 },
+    { category: '갈현동', lat: 37.417867, lng: 126.986083 },
+    { category: '갈현동', lat: 37.417504, lng: 126.986298 },
+    { category: '갈현동', lat: 37.416816, lng: 126.985189 },
+    { category: '갈현동', lat: 37.416021, lng: 126.98396 },
+    { category: '갈현동', lat: 37.415478, lng: 126.983951 },
+    { category: '갈현동', lat: 37.416724, lng: 126.982755 },
+    { category: '갈현동', lat: 37.416863, lng: 126.981945 },
+    { category: '갈현동', lat: 37.4159214552, lng: 126.979858 },
+    { category: '갈현동', lat: 37.417909, lng: 126.979304 },
+    { category: '갈현동', lat: 37.4040083811, lng: 126.9843401485 },
+    { category: '갈현동', lat: 37.404463, lng: 126.98082 },
+    { category: '갈현동', lat: 37.412139, lng: 126.982804 } // 새로운 위치 정보 추가
 ];
 
-const AInfo = [
+var AInfo = [
     {
         number: "A-GH-3",
         address: "원문동 1",
@@ -13,11 +29,139 @@ const AInfo = [
         image: "https://via.placeholder.com/150"
     },
     {
-        number: "A-GH-4",
+        number: "A-GH-4", // 새로운 관리번호 추가
         address: "원문동 2-1",
         rotation: 1,
         fixed: 4,
         description: "우체국사거리 2단지 소공원",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-6",
+        address: "갈현동 산121",
+        rotation: 1,
+        fixed: 2,
+        description: "12단",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-8",
+        address: "갈현동 640-8",
+        rotation: 1,
+        fixed: 0,
+        description: "모뎀말길 13 우정병원",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-9",
+        address: "갈현동 683-1",
+        rotation: 1,
+        fixed: 3,
+        description: "정보과학도서관 입구",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-10",
+        address: "갈현동 4-3",
+        rotation: 1,
+        fixed: 5,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-11",
+        address: "갈현동 8-55",
+        rotation: 1,
+        fixed: 1,
+        description: "놀이터 삼거",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-12",
+        address: "갈현동 8-5",
+        rotation: 1,
+        fixed: 0,
+        description: "찬우물공원내",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-13",
+        address: "갈현동 8-26",
+        rotation: 1,
+        fixed: 4,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-14",
+        address: "갈현동 14-4",
+        rotation: 1,
+        fixed: 2,
+        description: "가일길 한샘교회 앞",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-15",
+        address: "갈현동 산22-2",
+        rotation: 1,
+        fixed: 1,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-16",
+        address: "갈현동 산20-7",
+        rotation: 1,
+        fixed: 1,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-17",
+        address: "갈현동 357-5",
+        rotation: 1,
+        fixed: 2,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-18",
+        address: "갈현동 336-5",
+        rotation: 1,
+        fixed: 2,
+        description: "이운정 가든 ",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-19",
+        address: "갈현동 399",
+        rotation: 0,
+        fixed: 2,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-20",
+        address: "갈현동 274-4",
+        rotation: 1,
+        fixed: 1,
+        description: "엘라이트 근처",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-GH-21",
+        address: "갈현동 297-9",
+        rotation: 1,
+        fixed: 2,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "SPD-028",
+        address: "",
+        rotation: 1,
+        fixed: 3,
+        description: "지식정보타운",
         image: "https://via.placeholder.com/150"
     }
 ];

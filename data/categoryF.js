5a618ad5dd30de54da75e2335743bf3985c23e1b
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:26:49 +0900

    Create categoryF.js

---
+++ data/categoryF.js
@@ -0,0 +1,13 @@
+var Fpositions = [
+    { category: '주암동', lat: 37.430, lng: 127.013 }
+];
+var FInfo = [
+    {
+        number: "HP-116",
+        address: "주암동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    }
+];

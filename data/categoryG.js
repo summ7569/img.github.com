5c5fc61e3cb9acbcbfee25684aed447fe184a4f1
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:27:07 +0900

    Create categoryG.js

---
+++ data/categoryG.js
@@ -0,0 +1,13 @@
+var Gpositions = [
+    { category: '중앙동', lat: 37.433, lng: 126.999 }
+];
+var GInfo = [
+    {
+        number: "HP-117",
+        address: "중앙동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    }
+];

3c50a85581307faf08d29f09ff4031c6fa9d102b
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:27:26 +0900

    Create categoryH.js

---
+++ data/categoryH.js
@@ -0,0 +1,13 @@
+var Hpositions = [
+    { category: '기타', lat: 37.440, lng: 127.002 }
+];
+var HInfo = [
+    {
+        number: "HP-118",
+        address: "기타",
+        rotation: 1,
+        fixed: 0,
+        description: "기타 어린이집 앞",
+        image: "https://via.placeholder.com/150"
+    }
+];

4da882011f72846f7f551d4e312306b29783e143
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:18:26 +0900

    Create categoryA.js

---
+++ data/categoryA.js
@@ -0,0 +1,23 @@
+const Apositions = [
+    { category: '갈현동', lat: 37.4249270000, lng: 126.9897680000 },
+    { category: '갈현동', lat: 37.423681, lng: 126.9929 }
+];
+
+const AInfo = [
+    {
+        number: "A-GH-3",
+        address: "원문동 1",
+        rotation: 1,
+        fixed: 4,
+        description: "원문동1 소공원 지하철역 5번 출구",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-GH-4",
+        address: "원문동 2-1",
+        rotation: 1,
+        fixed: 4,
+        description: "우체국사거리 2단지 소공원",
+        image: "https://via.placeholder.com/150"
+    }
+];

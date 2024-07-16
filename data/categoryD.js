e0f084586761a57392c24623f2f719414fabcc2a
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:26:08 +0900

    Create categoryD.js

---
+++ data/categoryD.js
@@ -0,0 +1,13 @@
+var Dpositions = [
+    { category: '별양동', lat: 37.426, lng: 126.994 }
+];
+var DInfo = [
+    {
+        number: "HP-114",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    }
+];

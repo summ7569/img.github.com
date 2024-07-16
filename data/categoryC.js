d28d7add0175c6e5b26dd9c82f9d35504f61caa2
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-16 09:25:51 +0900

    Create categoryC.js

---
+++ data/categoryC.js
@@ -0,0 +1,419 @@
+var Cpositions = [
+    { category: '문원동', lat: 37.414653, lng: 126.992916 },
+    { category: '문원동', lat: 37.413015, lng: 126.994072 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 },
+    { category: '문원동', lat: 37.428, lng: 127.008 }
+    
+];
+
+var CInfo = [
+    {
+        number: "A-MW-8",
+        address: "문원동 902-11",
+        rotation: 1,
+        fixed: 0,
+        description: "세곡마을 입구",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-9",
+        address: "문원동 924-7번지",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-10",
+        address: "문원동 994-2",
+        rotation: 1,
+        fixed: 0,
+        description: "천궁수라상",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-11",
+        address: "문원동 1058",
+        rotation: 1,
+        fixed: 0,
+        description: "매봉 버스정류장",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-12",
+        address: "문원동 475-5",
+        rotation: 0,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-13",
+        address: "문원동 389",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-14",
+        address: "문원동 311",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-15",
+        address: "문원동 324-19",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-16",
+        address: "문원동 365",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-17",
+        address: "문원동 332-16",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-18",
+        address: "문원동 348",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-19",
+        address: "문원동 57-1",
+        rotation: 1,
+        fixed: 0,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-20",
+        address: "문원동 57-1",
+        rotation: 1,
+        fixed: 0,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-21",
+        address: "문원동 57-1",
+        rotation: 1,
+        fixed: 0,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-22",
+        address: "문원동 301(227-1)",
+        rotation: 0,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-23",
+        address: "문원동 227-2",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-24",
+        address: "문원동 217",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-25",
+        address: "문원동 199",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-26",
+        address: "참마을로 10-6",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-27",
+        address: "문원동 15-168",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-28",
+        address: "문원동 도로방범(102번지)",
+        rotation: 0,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-29",
+        address: "문원동 15-170",
+        rotation: 0,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-30",
+        address: "문원동 15-96",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-31",
+        address: "문원동 15-95",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-32",
+        address: "문원동 15-83",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-33",
+        address: "문원동 15-22,33",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-34",
+        address: "문원동 15-39",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-35",
+        address: "문원동 공원마을6길 21번지",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-36",
+        address: "문원동 15-146",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-37",
+        address: "문원동 142",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-38",
+        address: "문원동 115-21",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-39",
+        address: "문원동 115",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-40",
+        address: "문원동 115-113",
+        rotation: 1,
+        fixed: 1,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-41",
+        address: "문원동 115-81",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-42",
+        address: "문원동 115-174",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-43",
+        address: "문원동 115-214",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-44",
+        address: "문원동 150-1 대로",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-45",
+        address: "문원동 115-143",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-46",
+        address: "문원동 115-232",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-47",
+        address: "문원동 155-3",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-48",
+        address: "문원동 115-363",
+        rotation: 1,
+        fixed: 5,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-49",
+        address: "문원동 115-249",
+        rotation: 1,
+        fixed: 4,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-50",
+        address: "문원동 115-387",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-51",
+        address: "문원동 115-419",
+        rotation: 1,
+        fixed: 0,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-52",
+        address: "막계동 1013-7",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-MW-53",
+        address: "막계동 324-22",
+        rotation: 1,
+        fixed: 0,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    }
+];

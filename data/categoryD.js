066e0c4e5bfbc50ed96eee2c51281c9bce1bf800
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-17 09:51:31 +0900

    Update categoryD.js

--- data/categoryD.js
+++ data/categoryD.js
@@ -1,13 +1,278 @@
 var Dpositions = [
-    { category: '별양동', lat: 37.426, lng: 126.994 }
+    { category: '별양동', lat: 37.427748699, lng: 126.9913456668 },
+    { category: '별양동', lat: 37.427069, lng: 126.992345 },
+    { category: '별양동', lat: 37.4270009684, lng: 126.9930715182 },
+    { category: '별양동', lat: 37.4333993822, lng: 126.991496264 },
+    { category: '별양동', lat: 37.4310038373, lng: 126.9950879448 },
+    { category: '별양동', lat: 37.430038, lng: 126.995811 },
+    { category: '별양동', lat: 37.429667, lng: 126.997494 },
+    { category: '별양동', lat: 37.432088, lng: 126.995804 },
+    { category: '별양동', lat: 37.431379, lng: 126.997208 },
+    { category: '별양동', lat: 37.4334163884, lng: 126.9976837179 },
+    { category: '별양동', lat: 37.4331, lng: 126.9978 },
+    { category: '별양동', lat: 37.422973, lng: 126.994431 },
+    { category: '별양동', lat: 37.424382, lng: 126.994569 },
+    { category: '별양동', lat: 37.4239894447, lng: 126.9957690786 },
+    { category: '별양동', lat: 37.423566, lng: 126.996145 },
+    { category: '별양동', lat: 37.423557, lng: 126.997367 },
+    { category: '별양동', lat: 37.425096, lng: 126.995408 },
+    { category: '별양동', lat: 37.424462, lng: 126.996318 },
+    { category: '별양동', lat: 37.424460263, lng: 126.9969552992 },
+    { category: '별양동', lat: 37.426017, lng: 126.995724 },
+    { category: '별양동', lat: 37.42525, lng: 126.996663 },
+    { category: '별양동', lat: 37.426468, lng: 126.995745 },
+    { category: '별양동', lat: 37.426082, lng: 126.996407 },
+    { category: '별양동', lat: 37.425594, lng: 126.997457 },
+    { category: '별양동', lat: 37.4256, lng: 126.997862 },
+    { category: '별양동', lat: 37.4255212391, lng: 126.9983618261 },
+    { category: '별양동', lat: 37.427035, lng: 126.996547 },
+    { category: '별양동', lat: 37.4274223242, lng: 126.9958507962 },
+    { category: '별양동', lat: 37.423901, lng: 126.994833 },
+    { category: '별양동', lat: 37.423937, lng: 126.994872 }
 ];
 var DInfo = [
     {
-        number: "HP-114",
+        number: "A-BY-8",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BY-9",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-10",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-12",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-13",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-14",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-15",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-17",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-18",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-19",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-20",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-21",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-23",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-24",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-25",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-26",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-27",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-28",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-29",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-30",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-31",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-32",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-33",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-34",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-35",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-36",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-37",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-BY-38",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-177",
+        address: "별양동",
+        rotation: 1,
+        fixed: 3,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+        {
+        number: "A-178",
         address: "별양동",
         rotation: 1,
         fixed: 3,
         description: "어린이집",
         image: "https://via.placeholder.com/150"
     }
+
+
+
+    
 ];

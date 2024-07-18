647428a7d6337a2ec8a75f32d7f67b720540014d
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-18 13:10:47 +0900

    Update categoryE.js

--- data/categoryE.js
+++ data/categoryE.js
@@ -1,13 +1,194 @@
 var Epositions = [
-    { category: '부림동', lat: 37.429, lng: 127.000 }
+    { category: '부림동', lat: 37.4389598105, lng: 126.99511849 },
+    { category: '부림동', lat: 37.438125, lng: 126.995546 },
+    { category: '부림동', lat: 37.438615, lng: 126.99659 },
+    { category: '부림동', lat: 37.439158159, lng: 126.9973925711 },
+    { category: '부림동', lat: 37.437114, lng: 126.996613 },
+    { category: '부림동', lat: 37.43765341, lng: 126.99745194 },
+    { category: '부림동', lat: 37.438334, lng: 126.997586 },
+    { category: '부림동', lat: 37.438173768, lng: 126.9986920652 },
+    { category: '부림동', lat: 37.4358829098, lng: 126.9972118958 },
+    { category: '부림동', lat: 37.437557, lng: 126.998436 },
+    { category: '부림동', lat: 37.437116, lng: 126.999201 },
+    { category: '부림동', lat: 37.437444, lng: 126.999951 },
+    { category: '부림동', lat: 37.43651365, lng: 126.9991412454 },
+    { category: '부림동', lat: 37.4345179026, lng: 127.0004237163 },
+    { category: '부림동', lat: 37.4336033711, lng: 126.9995480413 },
+    { category: '부림동', lat: 37.4345179026, lng: 127.0004237164 },
+    { category: '부림동', lat: 37.435382, lng: 127.000983 },
+    { category: '부림동', lat: 37.434557, lng: 127.00168 },
+    { category: '부림동', lat: 37.433875, lng: 127.001884 },
+    { category: '부림동', lat: 37.4330514953, lng: 127.0009886521 },
+    { category: '부림동', lat: 37.430517, lng: 126.999614 }
 ];
 var EInfo = [
     {
-        number: "HP-115",
+        number: "A-BL-2",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-3",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-4",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-5",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-6",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-7",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-8",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-9",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-10",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-11",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-12",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-13",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-14",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-15",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-16",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-17",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-18",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-19",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-20",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-21",
+        address: "부림동",
+        rotation: 1,
+        fixed: 2,
+        description: "어린이집",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-BL-22",
         address: "부림동",
         rotation: 1,
         fixed: 2,
         description: "어린이집",
         image: "https://via.placeholder.com/150"
     }
+  
 ];

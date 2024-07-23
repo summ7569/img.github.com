4fb27c25f0c53ac6230c3449605abbe3a97cf2f0
Lican <67035981+summ7569@users.noreply.github.com>
2024-07-23 09:15:57 +0900

    Update categoryG.js

--- data/categoryG.js
+++ data/categoryG.js
@@ -1,13 +1,247 @@
 var Gpositions = [
-    { category: '중앙동', lat: 37.433, lng: 126.999 }
+    { category: '중앙동', lat: 37.429504, lng: 126.988322 },
+    { category: '중앙동', lat: 37.4299515199, lng: 126.9893201721 },
+    { category: '중앙동', lat: 37.430818, lng: 126.987814 },
+    { category: '중앙동', lat: 37.4316903414, lng: 126.9878623957 },
+    { category: '중앙동', lat: 37.431204, lng: 126.988663 },
+    { category: '중앙동', lat: 37.431986, lng: 126.987734 },
+    { category: '중앙동', lat: 37.4313572147, lng: 126.9905995419 },
+    { category: '중앙동', lat: 37.4324913933, lng: 126.9909072923 },
+    { category: '중앙동', lat: 37.4333507709, lng: 126.9913506713 },
+    { category: '중앙동', lat: 37.431935, lng: 126.989527 },
+    { category: '중앙동', lat: 37.432801, lng: 126.990281 },
+    { category: '중앙동', lat: 37.433241, lng: 126.98826 },
+    { category: '중앙동', lat: 37.433719, lng: 126.990165 },
+    { category: '중앙동', lat: 37.433938, lng: 126.991301 },
+    { category: '중앙동', lat: 37.433854, lng: 126.991964 },
+    { category: '중앙동', lat: 37.435082, lng: 126.993633 },
+    { category: '중앙동', lat: 37.435833, lng: 126.994325 },
+    { category: '중앙동', lat: 37.43633, lng: 126.993153 },
+    { category: '중앙동', lat: 37.437331, lng: 126.993819 },
+    { category: '중앙동', lat: 37.439524016, lng: 126.9939249153 },
+    { category: '중앙동', lat: 37.428468, lng: 126.990883 },
+    { category: '중앙동', lat: 37.4289989039, lng: 126.9919655156 },
+    { category: '중앙동', lat: 37.430032, lng: 126.991099 },
+    { category: '중앙동', lat: 37.433001, lng: 126.993452 },
+    { category: '중앙동', lat: 37.4334748513, lng: 126.9945454339 },
+    { category: '중앙동', lat: 37.433525, lng: 126.99515 },
+    { category: '중앙동', lat: 37.4348399481, lng: 126.9959760898 }
 ];
 var GInfo = [
     {
-        number: "HP-117",
-        address: "중앙동",
+        number: "A-JA-8",
+        address: "중앙동 24-12",
+        rotation: 1,
+        fixed: 3,
+        description: "향교말길 15-16",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-9",
+        address: "중앙동 25-7",
+        rotation: 1,
+        fixed: 4,
+        description: "향교말길 7-4",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-10",
+        address: "중앙동 7-9",
+        rotation: 1,
+        fixed: 3,
+        description: "향교말길 39",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-11",
+        address: "중앙동 9-1",
+        rotation: 1,
+        fixed: 1,
+        description: "과천외국어고등학교 앞 주택-미분류",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-12",
+        address: "중앙동 10-8",
         rotation: 1,
         fixed: 2,
-        description: "어린이집",
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-13",
+        address: "중앙동 11",
+        rotation: 1,
+        fixed: 0,
+        description: "과천외고 앞 등산로계단 밑",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-14",
+        address: "중앙동 33-13",
+        rotation: 1,
+        fixed: 3,
+        description: "1단지 옆",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-16",
+        address: "중앙동 29",
+        rotation: 1,
+        fixed: 2,
+        description: "교동경로당 놀이터",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-17",
+        address: "중앙동 19-7",
+        rotation: 1,
+        fixed: 3,
+        description: "자전거주차장 옆(1단지 근처)",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-18",
+        address: "중앙동 20-1",
+        rotation: 1,
+        fixed: 1,
+        description: "과천여고 정문 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-20",
+        address: "중앙동 19-1",
+        rotation: 1,
+        fixed: 2,
+        description: "희망4길 26",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-21",
+        address: "중앙동 80-1",
+        rotation: 1,
+        fixed: 4,
+        description: "관악산 등산로 입구",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-23",
+        address: "중앙동 14-1",
+        rotation: 1,
+        fixed: 3,
+        description: "",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-24",
+        address: "중앙동 17-4",
+        rotation: 1,
+        fixed: 2,
+        description: "희망4길 57",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-25",
+        address: "중앙동 71",
+        rotation: 1,
+        fixed: 1,
+        description: "1105동 ~ 1106동 사이",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-27",
+        address: "중앙동 74번지",
+        rotation: 1,
+        fixed: 0,
+        description: "래미안에코팰리스 1109 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-28",
+        address: "중앙동 67",
+        rotation: 1,
+        fixed: 3,
+        description: "과천초~1019 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-29",
+        address: "관문동 107-2",
+        rotation: 1,
+        fixed: 3,
+        description: "과천초-건강가정지원센터 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-30",
+        address: "중앙동 75",
+        rotation: 1,
+        fixed: 3,
+        description: "과천초 후문",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-31",
+        address: "중앙동 79",
+        rotation: 1,
+        fixed: 3,
+        description: "스타벅스 과천성당옆",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-33",
+        address: "중앙로 129",
+        rotation: 1,
+        fixed: 4,
+        description: "고려빌딩 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-35",
+        address: "중앙동 41-1",
+        rotation: 1,
+        fixed: 3,
+        description: "KT건물 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-36",
+        address: "중앙동 41-2",
+        rotation: 1,
+        fixed: 0,
+        description: "119동~120동 사이 관악산 등산로길",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-38",
+        address: "관문로 136",
+        rotation: 0,
+        fixed: 1,
+        description: "중앙동 주민센터 앞",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-39",
+        address: "중앙동 67",
+        rotation: 1,
+        fixed: 0,
+        description: "1007동~1002동사이",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-40",
+        address: "중앙동 67",
+        rotation: 1,
+        fixed: 0,
+        description: "1006동 앞 대로변",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        number: "A-JA-41",
+        address: "중앙동 67",
+        rotation: 1,
+        fixed: 0,
+        description: "1014동 옆 초소~상가사이",
         image: "https://via.placeholder.com/150"
     }
 ];
